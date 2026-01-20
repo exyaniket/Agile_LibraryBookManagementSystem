729657495dbf0f7660f8930b7b46a9d090c6fdb8
exyaniket <exyaniket@gmail.com>
2026-01-20 20:44:30 +0530

    BorrowReturnBook_spirint2

--- main.py
+++ main.py
@@ -18,3 +18,28 @@ def add_book():
 
     library[book_id]=Book(book_id,title,author)
     print("book added successfully");
+
+def borrow_book():
+    book_id=int(input("enter book id to borrow : "))
+    if book_id not in library:
+        print("book not found")
+        return
+    
+    library[book_id].is_borrowed=True
+    print("book borrowed successfully")
+
+def return_book():
+    book_id=int(input("enter bookid to return : "))
+    if book_id not in library:
+        print("book record not found")
+        return
+    
+    if not library[book_id].is_borrowed:
+        print("book not borrowed!!")
+        return
+    library[book_id].is_borrowed=False
+    print("book returned successfully")
+
+add_book()
+borrow_book()
+return_book()
