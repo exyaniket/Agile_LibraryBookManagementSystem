cc8181db2e92f995098e6f6cebc1076430d9df26
exyaniket <exyaniket@gmail.com>
2026-01-20 20:31:42 +0530

    Book Registration added

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ class Book:
         self.title = title
         self.author = author
         self.is_borrowed = False
+
+library = {}
+
+def add_book():
+    book_id=int(input("enter book id : "))
+    if book_id in library:
+        print("error : duplicate book !")
+        return
+    
+    title=input("enter title : ")
+    author=input("enter author: ")
+
+    library[book_id]=Book(book_id,title,author)
+    print("book added successfully");
